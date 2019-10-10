b33ad0ea7202e66963bb033b99459d9dd6988cb9
gaofeng <gaofeng@gaofeng>
2019-10-10 10:17:34 +0800

    fix the bug cannot send data to server and change topic to 'cam_test'

--- device.py
+++ device.py
@@ -67,39 +67,48 @@ class Device(object):
 
         num, matrix = read_anno_config(os.path.abspath(os.path.join(os.path.dirname(__file__), 'config/' + self.__urn[-12:] + '.json')))
 
-        # get camera instance
-        cam = ONVIFCamera(ip, port, '', '')
-        # create media service
-        media_service = cam.create_media_service()
-        token = '000'
-        # set video configuration
-        configurations_list = media_service.GetVideoEncoderConfigurations()
-        video_encoder_configuration = configurations_list[0]
-        options = media_service.GetVideoEncoderConfigurationOptions({'ProfileToken':token})
-        video_encoder_configuration.Encoding = 'H264'
-        video_encoder_configuration.Resolution = options.H264.ResolutionsAvailable[1]
-        for resolution in options.H264.ResolutionsAvailable:
-            if resolution["Width"] == 2592 and resolution["Height"] == 1944:
-                video_encoder_configuration.Resolution = resolution
-        request = media_service.create_type('SetVideoEncoderConfiguration')
-        request.Configuration = video_encoder_configuration
-        request.ForcePersistence = True
-        media_service.SetVideoEncoderConfiguration(request)
+        retry = 5
 
-        # get video stream
-        streamSetup = {
-            'StreamSetup': {
-                'Stream': 'RTP-Unicast',
-                'Transport': {
-                    'Protocol': 'TCP'
-                }
-            },
-            'ProfileToken': token
-        }
-        res = media_service.GetStreamUri(streamSetup)
-        self.__rtsp = cv2.VideoCapture(res.Uri)
+        while retry:
+            # get camera instance
+            cam = ONVIFCamera(ip, port, '', '')
+            # create media service
+            media_service = cam.create_media_service()
+            token = '000'
+            # set video configuration
+            configurations_list = media_service.GetVideoEncoderConfigurations()
+            video_encoder_configuration = configurations_list[0]
+            options = media_service.GetVideoEncoderConfigurationOptions({'ProfileToken':token})
+            video_encoder_configuration.Encoding = 'H264'
+            video_encoder_configuration.Resolution = options.H264.ResolutionsAvailable[1]
+            for resolution in options.H264.ResolutionsAvailable:
+                if resolution["Width"] == 2592 and resolution["Height"] == 1944:
+                    video_encoder_configuration.Resolution = resolution
+            request = media_service.create_type('SetVideoEncoderConfiguration')
+            request.Configuration = video_encoder_configuration
+            request.ForcePersistence = True
+            media_service.SetVideoEncoderConfiguration(request)
 
-        reporter = context.getContext().reporter
+            # get video stream
+            streamSetup = {
+                'StreamSetup': {
+                    'Stream': 'RTP-Unicast',
+                    'Transport': {
+                        'Protocol': 'TCP'
+                    }
+                },
+                'ProfileToken': token
+            }
+            res = media_service.GetStreamUri(streamSetup)
+            self.__rtsp = cv2.VideoCapture(res.Uri)
+
+            reporter = context.getContext().reporter
+            time.sleep(1)
+            retry = retry - 1
+            if self.__rtsp.isOpened():
+                print('--------------YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY camera opened')
+                break
+
         
         detect_result1 = {}
         detect_result2 = {}
@@ -124,11 +133,11 @@ class Device(object):
             detect_result = entry_detect(frame, num, matrix)
             print(detect_result)
             if detect_result == detect_result1 and detect_result == detect_result2 and detect_result == detect_result3 and detect_result == detect_result4:
-                #print('%s capture end %d. duration:%d' % (ip, time.time(), time.time() - start))
-                reporter.publish('hm_test', detect_result)
-            detect_result4 == detect_result3
-            detect_result3 == detect_result2
-            detect_result2 == detect_result1
+                print('*****************XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX%s capture end %d. duration:%d' % (ip, time.time(), time.time())) # - start))
+                reporter.publish('cam_test', detect_result)
+            detect_result4 = detect_result3
+            detect_result3 = detect_result2
+            detect_result2 = detect_result1
             detect_result1 = detect_result
             
             time.sleep(3)
